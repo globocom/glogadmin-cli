500d8734eebf5344901fa7194b3e4304001f3018
Sinval Vieira <sinvalneto01@gmail.com>
2016-09-23 18:13:41 -0300

    Adding import of roles

--- glogadmincli/cli.py
+++ glogadmincli/cli.py
@@ -4,6 +4,7 @@ import click
 
 from glogcli.graylog_api import GraylogAPIFactory
 from glogcli.utils import get_config
+from glogadmincli.graylog_api import GraylogAPI
 
 @click.command()
 @click.option("-sh", "--source-host", default=None, help="")
@@ -22,17 +23,40 @@ def main(source_host,
          target_password,
          source_port,
          target_port):
+
     print(source_host, target_host, source_username, target_username, source_password, target_password)
     click.echo("Replace this message by putting your code into "
                 "glogadmin-cli.cli.main")
     click.echo("See click documentation at http://click.pocoo.org/")
     cfg = get_config()
-    environment = None
-    source_api = GraylogAPIFactory.get_graylog_api(cfg, environment, source_host, source_password, source_port, None, False, source_username)
-    target_api = GraylogAPIFactory.get_graylog_api(cfg, environment, target_host, target_password, target_port, None, False, target_username)
 
-    print(source_api.get_saved_queries())
-    print(target_api.get_saved_queries())
+    source_api = GraylogAPIFactory.get_graylog_api(cfg, None, source_host, source_password, source_port, None, False, source_username)
+    target_api = GraylogAPIFactory.get_graylog_api(cfg, None, target_host, target_password, target_port, None, False, target_username)
+
+    source_api = GraylogAPI(source_api)
+    target_api = GraylogAPI(target_api)
+
+    source_roles = source_api.get_roles().get("roles")
+    target_roles = target_api.get_roles().get("roles")
+
+    roles_to_create = get_unique_roles_to_create(source_roles, target_roles)
+    print(roles_to_create)
+
+    for role in roles_to_create:
+        result = target_api.post_role(role)
+        print(result)
+
+def get_unique_roles_to_create(source_roles, target_roles):
+    for target_role in target_roles:
+        for source_role in source_roles:
+            if(source_role.get("name").lower() == target_role.get("name").lower()):
+                source_roles.remove(source_role)
+    return source_roles
+
+    #source_streams = source_api.get_streams()
+    #print(source_streams)
+    #target_api.post_streams(source_streams)
+
 
 if __name__ == "__main__":
     main()
--- glogadmincli/graylog_api.py
+++ glogadmincli/graylog_api.py
@@ -1,6 +1,36 @@
 
-from glogcli.graylog_api import GraylogAPI
+import click
+import requests
+import six
+import json
 
-class GraylogAPI(GraylogAPI):
-    def __init__(self):
-        pass
+
+class GraylogAPI(object):
+    def __init__(self, graylog_api):
+        self.graylog_api = graylog_api
+
+    def get_streams(self):
+        return self.graylog_api.streams()
+
+    def get_roles(self):
+        return self.graylog_api.get("roles")
+
+    def post_role(self, role, **kwargs):
+        url = "roles"
+        params = {}
+
+        for label, item in six.iteritems(kwargs):
+            if isinstance(item, list):
+                params[label + "[]"] = item
+            else:
+                params[label] = item
+
+        role['permissions'] = []
+        post_header = {"Accept":"application/json", "Content-Type": "application/json"}
+        r = requests.post(self.graylog_api.base_url + url, params=params, headers=post_header, auth=(self.graylog_api.username, self.graylog_api.password),
+                          proxies=self.graylog_api.proxies, data=json.dumps(role))
+        if r.status_code == requests.codes.ok:
+            return r.json()
+        else:
+            click.echo("API error: Status: {} Message: {}".format(r.status_code, r.content))
+            exit()

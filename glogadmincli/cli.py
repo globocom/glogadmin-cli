4df6617b2f7cfc0dac0389f8d13422400852ed53
Sinval Vieira <sinvalneto01@gmail.com>
2016-09-26 17:38:03 -0300

    Adding simple import of Roles and Streams

--- glogadmincli/cli.py
+++ glogadmincli/cli.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import click
+import json
 
 from glogcli.graylog_api import GraylogAPIFactory
 from glogcli.utils import get_config
@@ -15,6 +16,8 @@ from glogadmincli.graylog_api import GraylogAPI
 @click.option("-tp", "--target-password", default=None,  help="")
 @click.option("--source-port", default=80,  help="")
 @click.option("--target-port", default=80,  help="")
+@click.option("--import-roles", default=False, is_flag=True, help="")
+@click.option("--import-streams", default=False, is_flag=True, help="")
 def main(source_host,
          target_host,
          source_username,
@@ -22,12 +25,10 @@ def main(source_host,
          source_password,
          target_password,
          source_port,
-         target_port):
+         target_port,
+         import_roles,
+         import_streams):
 
-    print(source_host, target_host, source_username, target_username, source_password, target_password)
-    click.echo("Replace this message by putting your code into "
-                "glogadmin-cli.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
     cfg = get_config()
 
     source_api = GraylogAPIFactory.get_graylog_api(cfg, None, source_host, source_password, source_port, None, False, source_username)
@@ -36,15 +37,47 @@ def main(source_host,
     source_api = GraylogAPI(source_api)
     target_api = GraylogAPI(target_api)
 
-    source_roles = source_api.get_roles().get("roles")
-    target_roles = target_api.get_roles().get("roles")
+    if(import_roles):
+        source_roles = source_api.get_roles().get("roles")
+        target_roles = target_api.get_roles().get("roles")
 
-    roles_to_create = get_unique_roles_to_create(source_roles, target_roles)
-    print(roles_to_create)
+        roles_to_create = get_unique_roles_to_create(source_roles, target_roles)
 
-    for role in roles_to_create:
-        result = target_api.post_role(role)
-        print(result)
+        for role in roles_to_create:
+            result = target_api.post_role(role)
+            print result
+
+    if(import_streams):
+        source_streams = source_api.get_streams().get("streams")
+        i = 0
+        for stream in source_streams:
+            stream_rules = stream.get("rules")
+            for rule in stream_rules:
+                del rule["stream_id"]
+                del rule["id"]
+            click.echo("{}: {} , rules: {}".format(i, stream.get("description"), stream_rules))
+            i += 1
+        selected_streams_indexes = raw_input("Select a list of streams passing IDs splitted by space, like '1 4 7 18': ").split(' ')
+
+        created_streams = []
+        for selected_index in selected_streams_indexes:
+            stream_to_create = source_streams[int(selected_index)]
+            result = target_api.post_stream(stream_to_create)
+            created_streams.append(result)
+        click.echo("Streams created in the target: {}".format(created_streams))
+        click.echo("")
+
+        target_roles = target_api.get_roles().get("roles")
+        i = 0
+        for role in target_roles:
+            click.echo("{}: {} ".format(i, role.get("name")))
+            i += 1
+
+        selected_roles_indexes = raw_input("Select a list of roles to access the streams passing IDs splitted by space, like '1 4 7 18': ").split(' ')
+        for selected_index in selected_roles_indexes:
+            role_to_update = target_roles[int(selected_index)]
+            result = target_api.put_streams_in_role(role_to_update, created_streams)
+
 
 def get_unique_roles_to_create(source_roles, target_roles):
     for target_role in target_roles:
--- glogadmincli/graylog_api.py
+++ glogadmincli/graylog_api.py
@@ -4,8 +4,11 @@ import requests
 import six
 import json
 
+POST_DEFAULT_HEADER = {"Accept": "application/json", "Content-Type": "application/json"}
 
 class GraylogAPI(object):
+
+
     def __init__(self, graylog_api):
         self.graylog_api = graylog_api
 
@@ -26,11 +29,60 @@ class GraylogAPI(object):
                 params[label] = item
 
         role['permissions'] = []
-        post_header = {"Accept":"application/json", "Content-Type": "application/json"}
-        r = requests.post(self.graylog_api.base_url + url, params=params, headers=post_header, auth=(self.graylog_api.username, self.graylog_api.password),
+        r = requests.post(self.graylog_api.base_url + url, params=params, headers=POST_DEFAULT_HEADER,
+                          auth=(self.graylog_api.username, self.graylog_api.password),
                           proxies=self.graylog_api.proxies, data=json.dumps(role))
-        if r.status_code == requests.codes.ok:
+
+        if r.status_code == requests.codes.created:
             return r.json()
         else:
-            click.echo("API error: Status: {} Message: {}".format(r.status_code, r.content))
-            exit()
+            click.echo("API - Status: {} Message: {}".format(r.status_code, r.content))
+            return r.json()
+
+    def put_streams_in_role(self, role, streams, **kwargs):
+        url = "roles/{}".format(role.get("name"))
+        params = {}
+        for stream in streams:
+            role["permissions"].append("streams:read:{}".format(stream.get("stream_id")))
+            role["permissions"].append("streams:edit:{}".format(stream.get("stream_id")))
+
+        for label, item in six.iteritems(kwargs):
+            if isinstance(item, list):
+                params[label + "[]"] = item
+            else:
+                params[label] = item
+
+        r = requests.put(self.graylog_api.base_url + url, params=params, headers=POST_DEFAULT_HEADER,
+                         auth=(self.graylog_api.username, self.graylog_api.password),
+                         proxies=self.graylog_api.proxies, data=json.dumps(role))
+
+        click.echo("API - Status: {} Message: {}".format(r.status_code, r.content))
+        return r.json()
+
+    def post_stream(self, stream, **kwargs):
+        url = "streams"
+        params = {}
+
+        del stream['created_at']
+        del stream['creator_user_id']
+        del stream['id']
+        del stream['alert_conditions']
+        del stream['outputs']
+        del stream['disabled']
+        del stream['alert_receivers']
+
+        for label, item in six.iteritems(kwargs):
+            if isinstance(item, list):
+                params[label + "[]"] = item
+            else:
+                params[label] = item
+
+        r = requests.post(self.graylog_api.base_url + url, params=params, headers=POST_DEFAULT_HEADER,
+                          auth=(self.graylog_api.username, self.graylog_api.password),
+                          proxies=self.graylog_api.proxies, data=json.dumps(stream))
+
+        if r.status_code == requests.codes.created:
+            return r.json()
+        else:
+            click.echo("API - Status: {} Message: {}".format(r.status_code, r.content))
+            return r.json()
